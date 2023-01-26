3f88598f087cb19d7ac7f0bb2b4e54d8c90109e3
Andreas Primstad <andreaspri@live.no>
2023-01-26 14:10:21 +0000

    fixed the paths

--- ds_overview/ds_splitter.py
+++ ds_overview/ds_splitter.py
@@ -1,11 +1,14 @@
 import splitfolders as sf
 
-def split(from_path, to_path)
+def split(from_path, to_path, train, val, test)
     """Splits the dataset into train, val, and test where train is 80%, val is 10% and test is 10%
     
     from_path (str): Path to folder containing unsplit images
     to_path (str): Path to folder where the split images will be stored
+    train (float): Percentage of data to trainable data
+    val (float): Percentage of data to be validation data
+    test (float): Percentage of data to be test data
     
-    return: None
+    
     """
-    sf.ratio(from_path, to_path, seed=42, ratio=(0.8,0.1,0.1))
+    sf.ratio(from_path, to_path, seed=42, ratio=(train,val,test))
