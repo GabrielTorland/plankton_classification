26841bb8d6f5e3b357cceb3b7c15bc070ce86436
Andreas Primstad <andreaspri@live.no>
2023-01-26 14:02:22 +0000

    Docs for split and requirements updated

--- ds_overview/ds_splitter.py
+++ ds_overview/ds_splitter.py
@@ -1,3 +1,11 @@
 import splitfolders as sf
 
-sf.ratio("./../baseline_training_set/", "./../baseline_trainingset_split/", seed=42, ratio=(0.8,0.1,0.1))
+def split(from_path, to_path)
+    """Splits the dataset into train, val, and test where train is 80%, val is 10% and test is 10%
+    
+    from_path (str): Path to folder containing unsplit images
+    to_path (str): Path to folder where the split images will be stored
+    
+    return: None
+    """
+    sf.ratio("./../baseline_training_set/", "./../baseline_trainingset_split/", seed=42, ratio=(0.8,0.1,0.1))
