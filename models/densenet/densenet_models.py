fe700d27f9351597dd1617e2ae3adf64a228a428
Andreas Primstad <andreaspri@live.no>
2023-03-18 02:56:05 +0000

    first draft

---
+++ models/densenet/densenet_models.py
@@ -0,0 +1,135 @@
+from tensorflow.keras.applications import DenseNet121, DenseNet169, DenseNet201
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+
+
+DEFAULT_INPUT_SHAPE = (224, 224, 3)
+DEFAULT_WEIGHTS = "imagenet"
+
+
+def freeze_base_model(model):
+    """ Freeze the base model.
+
+    Args:
+        model (tensorflow.keras.applications.*): Base model. 
+    """    
+    for layer in model.layers:
+        layer.trainable = False
+
+
+def create_densenet121_model(classes, weights=DEFAULT_WEIGHTS, input_shape=DEFAULT_INPUT_SHAPE):
+    """Creates the DenseNet121 model.
+
+    Returns:
+        keras.engine.functional.Functional: The DenseNet201 model.
+    """
+    # Load the DenseNet121 model
+    base_model = DenseNet121(
+        include_top=False, # do not include the classification layer
+        weights=weights, # load pre-trained weights
+        input_shape=input_shape # specify input shape
+    )
+
+    # Freeze the base model
+    freeze_base_model(base_model)
+
+    # Add a global average pooling layer
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+
+    # Add a fully connected layer
+    x = Dense(1024, activation='relu')(x)
+    
+    # dropout layer to prevent overfitting
+    # 50% of the input will be dropped to zero
+    x = Dropout(0.5)(x)
+
+    # Add a classification layer
+    predictions = Dense(classes, activation='softmax')(x)
+
+    # Model to train
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+    # Compile the model
+    model.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
+    
+    return model
+
+
+def create_densenet169_model(classes, weights=DEFAULT_WEIGHTS, input_shape=DEFAULT_INPUT_SHAPE):
+    """Creates the DenseNet169 model.
+
+    Returns:
+        keras.engine.functional.Functional: The DenseNet201 model.
+    """
+    # Load the DenseNet169 model
+    base_model = DenseNet169(
+        include_top=False, # do not include the classification layer
+        weights=weights, # load pre-trained weights
+        input_shape=input_shape # specify input shape
+    )
+
+    # Freeze the base model
+    freeze_base_model(base_model)
+
+    # Add a global average pooling layer
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+
+    # Add a fully connected layer
+    x = Dense(1024, activation='relu')(x)
+    
+    # dropout layer to prevent overfitting
+    # 50% of the input will be dropped to zero
+    x = Dropout(0.5)(x)
+
+    # Add a classification layer
+    predictions = Dense(classes, activation='softmax')(x)
+
+    # Model to train
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+    # Compile the model
+    model.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
+    
+    return model
+
+
+def create_densenet201_model(classes, weights=DEFAULT_WEIGHTS, input_shape=DEFAULT_INPUT_SHAPE):
+    """Creates the DenseNet201 model.
+
+    Returns:
+        keras.engine.functional.Functional: The DenseNet201 model.
+    """
+    # Load the DenseNet201 model
+    base_model = DenseNet201(
+        include_top=False, # do not include the classification layer
+        weights=weights, # load pre-trained weights
+        input_shape=input_shape # specify input shape
+    )
+
+    # Freeze the base model
+    freeze_base_model(base_model)
+
+    # Add a global average pooling layer
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+
+    # Add a fully connected layer
+    x = Dense(1024, activation='relu')(x)
+    
+    # dropout layer to prevent overfitting
+    # 50% of the input will be dropped to zero
+    x = Dropout(0.5)(x)
+
+    # Add a classification layer
+    predictions = Dense(classes, activation='softmax')(x)
+
+    # Model to train
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+    # Compile the model
+    model.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
+    
+    return model
