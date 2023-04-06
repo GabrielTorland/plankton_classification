16d08e01b8a8c8a20f9d23cbbb000ebfe087fdf5
Andreas Primstad <andreaspri@live.no>
2023-04-06 03:30:53 +0000

    added functions for creating models without frozen layers

--- models/densenet/densenet_models.py
+++ models/densenet/densenet_models.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.applications import DenseNet121, DenseNet169, DenseNet201
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout, BatchNormalization
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 
@@ -18,6 +18,98 @@ def freeze_base_model(model):
         layer.trainable = False
 
 
+def create_densenet121_model_no_fc(classes, weights=DEFAULT_WEIGHTS, input_shape=DEFAULT_INPUT_SHAPE):
+    """Creates the DenseNet121 model.
+
+    Returns:
+        keras.engine.functional.Functional: The DenseNet121 model.
+    """
+    # Load the DenseNet121 model
+    base_model = DenseNet121(
+        include_top=False, # do not include the classification layer
+        weights=weights, # load pre-trained weights
+        input_shape=input_shape # specify input shape
+    )
+
+    # Add a global average pooling layer
+    x = base_model.output
+    
+    x= GlobalAveragePooling2D()(x)
+    
+    # Add a classification layer
+    predictions = Dense(classes, activation='softmax')(x)
+
+    # Model to train
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+    # Compile the model
+    model.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
+    
+    return model
+        
+
+    
+def create_densenet169_model_no_fc(classes, weights=DEFAULT_WEIGHTS, input_shape=DEFAULT_INPUT_SHAPE):
+    """Creates the DenseNet169 model.
+
+    Returns:
+        keras.engine.functional.Functional: The DenseNet169 model.
+    """
+    # Load the DenseNet169 model
+    base_model = DenseNet169(
+        include_top=False, # do not include the classification layer
+        weights=weights, # load pre-trained weights
+        input_shape=input_shape # specify input shape
+    )
+
+    # Add a global average pooling layer
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    
+    # Add a classification layer
+    predictions = Dense(classes, activation='softmax')(x)
+
+    # Model to train
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+    # Compile the model
+    model.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
+    
+    return model
+
+    
+def create_densenet201_model_no_fc(classes, weights=DEFAULT_WEIGHTS, input_shape=DEFAULT_INPUT_SHAPE):
+    """Creates the DenseNet201 model.
+
+    Returns:
+        keras.engine.functional.Functional: The DenseNet201 model.
+    """
+    # Load the DenseNet201 model
+    base_model = DenseNet201(
+        include_top=False, # do not include the classification layer
+        weights=weights, # load pre-trained weights
+        input_shape=input_shape # specify input shape
+    )
+
+    # Add a global average pooling layer
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    
+    # Add a classification layer
+    predictions = Dense(classes, activation='softmax')(x)
+
+    # Model to train
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+    # Compile the model
+    model.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
+    
+    return model
+   
+
+    
+    
+    
 def create_densenet121_model(classes, weights=DEFAULT_WEIGHTS, input_shape=DEFAULT_INPUT_SHAPE):
     """Creates the DenseNet121 model.
 
@@ -36,14 +128,26 @@ def create_densenet121_model(classes, weights=DEFAULT_WEIGHTS, input_shape=DEFAU
 
     # Add a global average pooling layer
     x = base_model.output
-    x = GlobalAveragePooling2D()(x)
+    
+    x= GlobalAveragePooling2D()(x)
+    
+    # Batch normalization
+    x = BatchNormalization()(x)
+    
+    # dropout layer to prevent overfitting
+    # 50% of the input will be dropped to zero
+    x= Dropout(0.5)(x)
 
-    # Add a fully connected layer
-    x = Dense(1024, activation='relu')(x)
+    # Add two fully connected layer
+    x= Dense(1024,activation='relu')(x)
+    x= Dense(512,activation='relu')(x) 
     
     # dropout layer to prevent overfitting
     # 50% of the input will be dropped to zero
     x = Dropout(0.5)(x)
+    
+    # Batch normalization
+    x = BatchNormalization()(x)
 
     # Add a classification layer
     predictions = Dense(classes, activation='softmax')(x)
@@ -75,14 +179,26 @@ def create_densenet169_model(classes, weights=DEFAULT_WEIGHTS, input_shape=DEFAU
 
     # Add a global average pooling layer
     x = base_model.output
-    x = GlobalAveragePooling2D()(x)
+    
+    x= GlobalAveragePooling2D()(x)
+    
+    # Batch normalization
+    x = BatchNormalization()(x)
+    
+    # dropout layer to prevent overfitting
+    # 50% of the input will be dropped to zero
+    x= Dropout(0.5)(x)
 
-    # Add a fully connected layer
-    x = Dense(1024, activation='relu')(x)
+    # Add two fully connected layer
+    x= Dense(1024,activation='relu')(x)
+    x= Dense(512,activation='relu')(x) 
     
     # dropout layer to prevent overfitting
     # 50% of the input will be dropped to zero
     x = Dropout(0.5)(x)
+    
+    # Batch normalization
+    x = BatchNormalization()(x)
 
     # Add a classification layer
     predictions = Dense(classes, activation='softmax')(x)
@@ -94,6 +210,7 @@ def create_densenet169_model(classes, weights=DEFAULT_WEIGHTS, input_shape=DEFAU
     model.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
     
     return model
+
 
 
 def create_densenet201_model(classes, weights=DEFAULT_WEIGHTS, input_shape=DEFAULT_INPUT_SHAPE):
@@ -114,14 +231,26 @@ def create_densenet201_model(classes, weights=DEFAULT_WEIGHTS, input_shape=DEFAU
 
     # Add a global average pooling layer
     x = base_model.output
-    x = GlobalAveragePooling2D()(x)
+    
+    x= GlobalAveragePooling2D()(x)
+    
+    # Batch normalization
+    x = BatchNormalization()(x)
+    
+    # dropout layer to prevent overfitting
+    # 50% of the input will be dropped to zero
+    x= Dropout(0.5)(x)
 
-    # Add a fully connected layer
-    x = Dense(1024, activation='relu')(x)
+    # Add two fully connected layer
+    x= Dense(1024,activation='relu')(x)
+    x= Dense(512,activation='relu')(x) 
     
     # dropout layer to prevent overfitting
     # 50% of the input will be dropped to zero
     x = Dropout(0.5)(x)
+    
+    # Batch normalization
+    x = BatchNormalization()(x)
 
     # Add a classification layer
     predictions = Dense(classes, activation='softmax')(x)
